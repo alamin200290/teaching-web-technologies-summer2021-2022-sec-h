281482eed668cf2ec769c8a19febeca83afbaa19
alamin200290c <al.amin@crowdbotics.com>
2022-06-19 13:51:31 +0600

    include/require

--- login_system/userlist.php
+++ login_system/userlist.php
@@ -1,7 +1,10 @@
 <?php 
-	if(!isset($_COOKIE['status'])){
-		header('location: login.html');
-	}
+	include('header.php');
+	include_once 'header.php';
+
+	require('header.php');
+	require_once('header.php');
+
 ?>
 
 <!DOCTYPE html>
